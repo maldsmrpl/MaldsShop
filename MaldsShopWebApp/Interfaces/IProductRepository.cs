77016c1c4fda0e4842bd3454e34e4df09d3771bb
malds <malkov.dmytro@gmail.com>
2024-03-07 18:44:26 +0100

    ProductRepository Fix

--- MaldsShopWebApp/Interfaces/IProductRepository.cs
+++ MaldsShopWebApp/Interfaces/IProductRepository.cs
@@ -5,7 +5,7 @@
         public bool Add(Product product);
         public bool Update(Product product);
         public bool Delete(Product product);
-        public bool Save(Product product);
+        public bool Save();
         public IEnumerable<Product> GetAll();
         public Task<IEnumerable<Product>> GetAllAsync();
     }

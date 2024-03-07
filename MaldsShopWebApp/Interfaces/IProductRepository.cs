77a97286b387c0a8d94139d4939ca4cca853ed6d
malds <malkov.dmytro@gmail.com>
2024-03-07 18:43:03 +0100

    ProductRepository

---
+++ MaldsShopWebApp/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MaldsShopWebApp.Interfaces
+{
+    public interface IProductRepository
+    {
+        public bool Add(Product product);
+        public bool Update(Product product);
+        public bool Delete(Product product);
+        public bool Save(Product product);
+        public IEnumerable<Product> GetAll();
+        public Task<IEnumerable<Product>> GetAllAsync();
+    }
+}
---
+++ MaldsShopWebApp/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using MaldsShopWebApp.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaldsShopWebApp.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Product product)
+        {
+            _context.Products.Add(product);
+            return Save();
+        }
+        public bool Update(Product product)
+        {
+            _context.Products.Update(product);
+            return Save();
+        }
+        public bool Delete(Product product)
+        {
+            _context.Products.Remove(product);
+            return Save();
+        }
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}

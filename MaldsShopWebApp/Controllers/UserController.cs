e6fff8ca2a5889e4a5a6ac780513cff211d8101a
malds <malkov.dmytro@gmail.com>
2024-03-22 17:30:44 +0100

    User dashboard

--- MaldsShopWebApp/Controllers/UserController.cs
+++ MaldsShopWebApp/Controllers/UserController.cs
@@ -1,22 +1,55 @@
 ﻿using MaldsShopWebApp.Interfaces;
 using MaldsShopWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MaldsShopWebApp.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IReviewRepository _reviewRepository;
+        private readonly IUserRepository _userRepository;
 
-        public UserController(IOrderRepository orderRepository, IReviewRepository reviewRepository)
+        public UserController(IOrderRepository orderRepository, IReviewRepository reviewRepository, IUserRepository userRepository)
         {
             _orderRepository = orderRepository;
             _reviewRepository = reviewRepository;
+            _userRepository = userRepository;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var userEmail = User?.Identity?.Name;
+
+            if (User.Identity.IsAuthenticated && userEmail != null)
+            {
+                var user = await _userRepository.GetByEmailAsync(userEmail);
+                var orders = await _orderRepository.GetOrdersByUserEmailAsync(userEmail);
+                var reviews = await _reviewRepository.GetAllByUserEmailAsync(userEmail);
+
+                UserIndexViewModel userVM = new UserIndexViewModel()
+                {
+                    UserEmail = userEmail,
+                    AppUser = user,
+                    Orders = orders.Take(5),
+                    Reviews = reviews.Take(5),
+                };
+                return View(userVM);
+            }
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserIndexViewModel userVM)
+        {
+            var user = await _userRepository.GetByEmailAsync(userVM.UserEmail);
+            if (user != null)
+            {
+                user.FirstName = userVM.FirstName;
+                user.LastName = userVM.LastName;
+                _userRepository.Update(user);
+            }
+            return RedirectToAction("Index", "User");
         }
         public async Task<IActionResult> Orders()
         {
---
+++ MaldsShopWebApp/ViewModels/UserIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaldsShopWebApp.ViewModels
+{
+    public class UserIndexViewModel
+    {
+        [Required(ErrorMessage = "First name required")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Last name required")]
+        public string? LastName { get; set; }
+        public string UserEmail { get; set; }
+        public AppUser AppUser { get; set; }
+        public IEnumerable<Order>? Orders { get; set; }
+        public IEnumerable<Review>? Reviews { get; set; }
+    }
+}
